4e47f6df492064d71c500f7f8197ff1ed1406f09
Stuart Millman <stu.millman15@gmail.com>
2023-11-14 20:59:43 +0000

    Add shaderc submodule (+ deps).

--- src/VkMana/ShaderCompiler.cpp
+++ src/VkMana/ShaderCompiler.cpp
@@ -1,23 +1,53 @@
 #include "ShaderCompiler.hpp"
 
+#include <shaderc/shaderc.hpp>
+
 namespace VkMana
 {
-	bool CompileShader(
-		ShaderBinary& outSpirv, const std::string& glslSource, shaderc_shader_kind kind, bool debug, const std::string& filename)
+	namespace
 	{
+		auto GetShaderKind(const vk::ShaderStageFlagBits shaderStage) -> shaderc_shader_kind
+		{
+			switch (shaderStage)
+			{
+				case vk::ShaderStageFlagBits::eVertex:
+					return shaderc_vertex_shader;
+				case vk::ShaderStageFlagBits::eTessellationControl:
+					return shaderc_tess_control_shader;
+				case vk::ShaderStageFlagBits::eTessellationEvaluation:
+					return shaderc_tess_evaluation_shader;
+				case vk::ShaderStageFlagBits::eGeometry:
+					return shaderc_geometry_shader;
+				case vk::ShaderStageFlagBits::eFragment:
+					return shaderc_fragment_shader;
+				case vk::ShaderStageFlagBits::eCompute:
+					return shaderc_compute_shader;
+				default:
+					LOG_ERR("Unknown shader kind.");
+					assert(false);
+					return {};
+			}
+		}
+
+	} // namespace
+
+	bool CompileShader(
+		ShaderBinary& outSpirv, const std::string& glslSource, vk::ShaderStageFlagBits shaderStage, bool debug, const std::string& filename)
+	{
+		assert(!filename.empty());
+
 		shaderc::CompileOptions options;
 		if (debug)
 			options.SetGenerateDebugInfo();
 		else
 			options.SetOptimizationLevel(shaderc_optimization_level_performance);
 
-		options.SetSourceLanguage(shaderc_source_language_glsl);
-
+		auto kind = GetShaderKind(shaderStage);
 		shaderc::Compiler compiler;
 		auto result = compiler.CompileGlslToSpv(glslSource, kind, filename.c_str(), options);
 		if (result.GetCompilationStatus() != shaderc_compilation_status_success)
 		{
-			LOG_ERR("Shader Compile Error: {}", result.GetErrorMessage());
+			LOG_ERR("Shader Compile Error: \n{}", result.GetErrorMessage());
 			return false;
 		}
 
--- src/VkMana/ShaderCompiler.hpp
+++ src/VkMana/ShaderCompiler.hpp
@@ -3,13 +3,14 @@
 #include "Vulkan_Common.hpp"
 #include "Pipeline.hpp"
 
-#include <shaderc/shaderc.hpp>
-
 #include <string>
 
 namespace VkMana
 {
-	bool CompileShader(
-		ShaderBinary& outSpirv, const std::string& glslSource, shaderc_shader_kind, bool debug, const std::string& filename = "");
+	bool CompileShader(ShaderBinary& outSpirv,
+		const std::string& glslSource,
+		vk::ShaderStageFlagBits shaderStage,
+		bool debug,
+		const std::string& filename);
 
 } // namespace VkMana

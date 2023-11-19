43268bb1801c800b1f4d95ad33205a9cafc2c4ef
Stuart Millman <stu.millman15@gmail.com>
2023-11-19 07:33:32 +0000

    WSI cursor funcs.

--- src/VkMana/WSI.hpp
+++ src/VkMana/WSI.hpp
@@ -16,5 +16,11 @@ namespace VkMana
 		virtual auto GetSurfaceHeight() -> uint32_t = 0;
 		virtual bool IsVSync() = 0;
 		virtual bool IsAlive() = 0;
+
+		virtual void HideCursor() = 0;
+		virtual void ShowCursor() = 0;
+
+		virtual auto CreateCursor(uint32_t cursorType) -> void* = 0;
+		virtual void SetCursor(void* cursor) = 0;
 	};
 } // namespace VkMana

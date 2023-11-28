624b7e6d1d61bf5fd0ff71d0db29676ac729abf9
Stuart Millman <stu.millman15@gmail.com>
2023-11-28 15:22:56 +0000

    Remove vma-hpp.

--- third_party/vma-hpp/cmake/VulkanMemoryAllocator-HppConfig.cmake
+++
@@ -1,6 +0,0 @@
-include (CMakeFindDependencyMacro)
-
-find_dependency (Vulkan REQUIRED)
-find_dependency (VulkanMemoryAllocator REQUIRED)
-
-include (${CMAKE_CURRENT_LIST_DIR}/VulkanMemoryAllocator-HppTargets.cmake)

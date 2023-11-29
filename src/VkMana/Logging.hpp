39925a81c1a4fa69ca405d368483934220a5826c
Stuart Millman <stu.millman15@gmail.com>
2023-11-29 09:24:39 +0000

    Add fmtlib submodule.

--- src/VkMana/Logging.hpp
+++ src/VkMana/Logging.hpp
@@ -1,26 +1,26 @@
 #pragma once
 
-#include <cstdio>
-#include <format>
+#include <fmt/format.h>
+
 #include <iostream>
 
 #define VM_ERR(...)                                                      \
 	do                                                                    \
 	{                                                                     \
-		std::cerr << "[ERROR] " << std::format(__VA_ARGS__) << std::endl; \
+		std::cerr << "[ERROR] " << fmt::format(__VA_ARGS__) << std::endl; \
 	}                                                                     \
 	while (false)
 
 #define VM_WARN(...)                                                    \
 	do                                                                   \
 	{                                                                    \
-		std::cout << "[WARN] " << std::format(__VA_ARGS__) << std::endl; \
+		std::cout << "[WARN] " << fmt::format(__VA_ARGS__) << std::endl; \
 	}                                                                    \
 	while (false)
 
 #define VM_INFO(...)                                                    \
 	do                                                                   \
 	{                                                                    \
-		std::cout << "[INFO] " << std::format(__VA_ARGS__) << std::endl; \
+		std::cout << "[INFO] " << fmt::format(__VA_ARGS__) << std::endl; \
 	}                                                                    \
 	while (false)
